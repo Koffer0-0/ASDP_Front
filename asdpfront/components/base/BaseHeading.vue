fc176e93340e46009e8409821ac326d75ed7642e
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-15 14:37:35 +0500

    Create BaseHeading.vue

---
+++ asdpfront/components/base/BaseHeading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+    default: "Heading"
+  }
+})
+</script>
+
+<template>
+  <div class="flex gap-1 items-center ">
+    <h5 class="text-xl font-semibold dark:text-white">{{ text }}</h5>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

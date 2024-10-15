21dcd570ec55c95f5292d7a2a78a8a7dc51afa9d
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-15 14:37:30 +0500

    adjust style of the main content

--- asdpfront/app.vue
+++ asdpfront/app.vue
@@ -2,10 +2,8 @@
   <div>
     <the-navbar/>
     <the-sidebar/>
-    <main class="p-4 md:ml-64 h-full pt-20 bg-gray-100">
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-      <NuxtPage />
-      </div>
+    <main class="p-4 md:ml-64 h-screen pt-20 bg-gray-100">
+        <NuxtPage />
     </main>
   </div>
 </template>

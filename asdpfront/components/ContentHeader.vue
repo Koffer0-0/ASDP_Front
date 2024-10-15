f8e970faa9b2467f994ee528e9435caa86c0058d
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-15 14:37:42 +0500

    Create ContentHeader.vue

---
+++ asdpfront/components/ContentHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: "Heading"
+  }
+})
+</script>
+
+<template>
+  <header class="border-b border-gray-200 mb-3 pb-4">
+    <div class="mx-auto max-w-screen-xl px-4">
+      <div class="flex flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
+        <BaseHeading :text="title"/>
+        <slot name="button"/>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>

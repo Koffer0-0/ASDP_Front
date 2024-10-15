9022717aa6327adfc0bfb9e3937090718b0ce554
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-15 11:01:01 +0500

    Form the grid of website

--- asdpfront/app.vue
+++ asdpfront/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <the-navbar/>
+    <the-sidebar/>
+    <main class="p-4 md:ml-64 h-full pt-20 bg-gray-100">
+      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+      <NuxtPage />
+      </div>
+    </main>
   </div>
 </template>
+
+<script setup lang="ts">
+onMounted(() => {
+  localStorage.setItem('theme', 'light');
+})
+</script>

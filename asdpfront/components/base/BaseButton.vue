ec866854192b68cb3882587c8b89d1615794718e
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-15 14:37:39 +0500

    Create BaseButton.vue

---
+++ asdpfront/components/base/BaseButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+  icon: {
+
+  },
+  text: {
+    type: String,
+    required: true,
+    default: "Text Button"
+  }
+})
+</script>
+
+<template>
+  <button type="button"
+          class="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-md bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+    <component :is="icon"/>
+      {{ text }}
+  </button>
+</template>
+
+<style scoped>
+
+</style>

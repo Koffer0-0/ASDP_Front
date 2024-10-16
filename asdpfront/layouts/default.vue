b65be7ad3780eb1037621ce8f431619a79b026c6
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:02 +0500

    default layout

---
+++ asdpfront/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <the-navbar/>
+    <the-sidebar/>
+      <main class="p-4 md:ml-64 h-screen pt-20 bg-gray-100">
+        <NuxtPage />
+      </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

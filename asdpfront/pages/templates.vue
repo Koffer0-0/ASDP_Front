00b0faaccc52f1a2d8b2cbbde532a1a809362c77
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:39 +0500

    Create templates.vue

---
+++ asdpfront/pages/templates.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const templates = [
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+  {id: "", name: "", description: "", img: ""},
+]
+</script>
+
+<template>
+  <ContentHeader title="Шаблоны">
+    <template #button>
+      <BaseButton text="Добавить Шаблон"/>
+    </template>
+  </ContentHeader>
+  <main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+    <TemplateCard v-for="t in templates"/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>

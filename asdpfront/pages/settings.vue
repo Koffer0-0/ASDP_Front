ed79917761dd5ecf51184b2955b6e3bcbcbc0866
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:31 +0500

    Create settings.vue

---
+++ asdpfront/pages/settings.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <<ContentHeader title="Документы">>
+    <template #button>
+      <BaseButton text="Test"/>
+    </template>
+  </ContentHeader>
+</template>
+
+<style scoped>
+
+</style>

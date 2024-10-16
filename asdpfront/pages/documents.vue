897b21d9990370f9fd0960377370db99a3d7100e
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:12 +0500

    Create documents.vue

---
+++ asdpfront/pages/documents.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <ContentHeader title="Документы">
+      <template #button>
+        <BaseButton text="Test"/>
+      </template>
+    </ContentHeader>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

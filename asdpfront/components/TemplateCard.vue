114092325a9a8a7f7e526eb1f2593d3266c5591c
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:43:13 +0500

    Create TemplateCard.vue

---
+++ asdpfront/components/TemplateCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+      <img width="360" height="280" class="max-w-lg max-h-72 w-full h-auto object-contain rounded-t-lg" src="https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_4cda85d892a5c0b5dd63b510a9c83e9c9d06e739.jpg" alt="" />
+    </a>
+    <div class="p-5 flex flex-col items-center justify-center text-center">
+      <a href="#">
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Шаблон</h5>
+      </a>
+      <p class="mb-3 text-sm font-normal text-gray-700 dark:text-gray-400">Описание Шаблона</p>
+      <a href="#" class="px-5 py-2 font-medium text-center text-gray-500 border-2 border-gray-400 rounded-md focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Изменить
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

e11f157c3984584ef2ae0d9c80cdce99c71aaec6
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:36 +0500

    Create structure.vue

---
+++ asdpfront/pages/structure.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ContentHeader title="Структура"/>
+  <span class="bg-gray-200 w-80 h-40 flex justify-center">В разработке</span>
+</template>
+
+<style scoped>
+
+</style>

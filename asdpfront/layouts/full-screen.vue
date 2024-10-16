fd62e993b37639b66320869c51aae4c6920e3331
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-16 18:44:08 +0500

    full screen layout

---
+++ asdpfront/layouts/full-screen.vue
@@ -0,0 +1,13 @@
+<!-- layouts/full-screen.vue -->
+<template>
+  <div class="h-screen bg-gray-100 flex items-center justify-center">
+    <!-- NuxtPage renders the specific page content -->
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  localStorage.setItem('theme', 'light');
+});
+</script>

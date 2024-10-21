1e4b1076992e6922c832c227b3ea39ec44e3c4a3
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-21 16:53:14 +0500

    Refactor input

--- asdpfront/components/base/BaseInput.vue
+++ asdpfront/components/base/BaseInput.vue
@@ -1,16 +1,47 @@
-<script setup lang="ts">
-defineProps({
-
-})
-</script>
-
 <template>
   <div>
-    <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
-    <input type="text" name="brand" id="brand" value="Google" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Ex. Apple">
+    <label :for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+      {{ label }}
+    </label>
+    <input
+        :type="type"
+        :name="name"
+        :id="id"
+        :value="value"
+        :placeholder="placeholder"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+        @input="$emit('update:value', $event.target.value)"
+    />
   </div>
 </template>
 
-<style scoped>
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'text'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: [String, Number],
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+});
 
-</style>
+defineEmits(['update:value']);
+</script>

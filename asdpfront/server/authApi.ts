d925aef46bab4eb2c0e5c7239762720a18c6ece7
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 10:35:44 +0500

    Create authApi.ts

---
+++ asdpfront/server/authApi.ts
@@ -0,0 +1,15 @@
+
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_APP_API;
+
+export const getEmployeeByIin = async (iin: string) => {
+  try {
+    const response = await axios.get(`${API_URL}Authorize/getEmployeeByIin`, {
+      params: { iin }
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};

699ac71b7d0b071ab74c44393375b2c306d735ce
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 12:31:06 +0500

    Prepare selects for employee endpoint

---
+++ asdpfront/pages/form_document/[id].vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+const {
+  positions,
+  directors,
+  permissions,
+  teamleaders,
+  employees,
+  fetchPermissions,
+  fetchDirectors,
+  fetchTeamleaders,
+  fetchEmployees,
+  fetchPositions
+} = useEmployee()
+
+const data = ref({
+  position: "",
+  director: "",
+  permission: "",
+  teamlead: "",
+  employee: "",
+})
+
+onMounted(() => {
+  fetchPermissions()
+  fetchDirectors()
+  fetchTeamleaders()
+  fetchEmployees()
+  fetchPositions()
+})
+</script>
+
+<template>
+  Вот это Для чего?
+  <BaseSelect :options="positions"
+              v-model="data.position"
+              defaultOption="Выберите Позицию" />
+  <BaseSelect :options="directors"
+              v-model="data.director"
+              defaultOption="Выберите Директора" />
+  <BaseSelect :options="permissions"
+              v-model="data.permission"
+              defaultOption="Выберите Доступ" />
+  <BaseSelect :options="teamleaders"
+              v-model="data.teamlead"
+              defaultOption="Выберите Тимлида" />
+  <BaseSelect :options="employees"
+              v-model="data.employee"
+              defaultOption="Выберите Сотрудника" />
+</template>
+
+<style scoped>
+
+</style>

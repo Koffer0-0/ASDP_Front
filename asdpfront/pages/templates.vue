7a2a9d029e38c3554453975fd97ca23de7a47e69
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 11:40:52 +0500

    Implement uploading templates

--- asdpfront/pages/templates.vue
+++ asdpfront/pages/templates.vue
@@ -1,25 +1,63 @@
 <script setup lang="ts">
-const templates = [
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-  {id: "", name: "", description: "", img: ""},
-]
+import { useDocument } from "~/composables/useDocument";
+
+const {fetchTemplates, templates, createTemplate} = useDocument()
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+onMounted(() => {
+  fetchTemplates()
+})
+
+
+const handleUploadTemplate = () => {
+  fileInput.value?.click();
+};
+
+const onFileChange = async (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const file = target?.files?.[0];
+
+  if (!file) {
+    alert("No file selected");
+    return;
+  }
+
+  try {
+    const fileName = file.name.split('.').slice(0, -1).join('.');
+    // Create FormData to send the file to the createTemplate function
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("name", fileName); // Append the file name separately
+
+    // Call createTemplate to upload the file
+    await createTemplate(formData);
+
+  } catch (error) {
+    console.error("Error during file upload:", error);
+    alert("File upload failed");
+  }
+
+  // Reset the file input after uploading
+  target.value = "";
+};
 </script>
 
 <template>
   <ContentHeader title="Шаблоны">
     <template #button>
-      <BaseButton text="Добавить Шаблон"/>
+      <BaseButton text="Добавить Шаблон" @click="handleUploadTemplate"/>
     </template>
   </ContentHeader>
   <main class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
-    <TemplateCard v-for="t in templates"/>
+    <TemplateCard v-for="t in templates" :template="t"/>
+    <input
+        type="file"
+        ref="fileInput"
+        class="hidden"
+        accept=".doc, .docx"
+        @change="onFileChange"
+    />
   </main>
 </template>
 

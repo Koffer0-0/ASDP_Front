fa901e84fc0ac9fe9d1d1ac21906568810fbe172
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 12:38:50 +0500

    load user from store

--- asdpfront/app.vue
+++ asdpfront/app.vue
@@ -5,7 +5,12 @@
 </template>
 
 <script setup lang="ts">
+const userStore = useUserStore();
+
 onMounted(() => {
   localStorage.setItem('theme', 'light');
+
+  userStore.loadUser(); // Load user data into Pinia store on startup
 })
+
 </script>

619eced80c8e182dd8c57d72091471322060caa8
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 11:25:23 +0500

    fix the api call function url params

--- asdpfront/server/authApi.ts
+++ asdpfront/server/authApi.ts
@@ -5,9 +5,7 @@ const API_URL = import.meta.env.VITE_APP_API;
 
 export const getEmployeeByIin = async (iin: string) => {
   try {
-    const response = await axios.get(`${API_URL}Authorize/getEmployeeByIin`, {
-      params: { iin }
-    });
+    const response = await axios.get(`${API_URL}Authorize/getEmployeeByIin?iin=${iin}`);
     return response.data;
   } catch (error) {
     throw error;

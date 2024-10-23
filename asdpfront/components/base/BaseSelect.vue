59a518dbb02faf044bbd7e3897d186920aa29411
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-23 12:30:47 +0500

    Create BaseSelect.vue

---
+++ asdpfront/components/base/BaseSelect.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+defineProps({
+  name: {
+    type: String
+  },
+  defaultOption: {
+    type: String
+  },
+  options: {
+    type: Array
+  }
+})
+</script>
+
+<template>
+  <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{name}}</label>
+  <select :id="name"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option selected>{{ defaultOption }}</option>
+    <option v-for="o in options"
+            :value="o.шв">{{o.name}}</option>
+  </select>
+</template>
+
+<style scoped>
+
+</style>

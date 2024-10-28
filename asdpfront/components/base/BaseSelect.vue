e22851adb9b40f836970f1ac09b297d6eda3e32b
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-28 11:59:30 +0500

    Update BaseSelect.vue

--- asdpfront/components/base/BaseSelect.vue
+++ asdpfront/components/base/BaseSelect.vue
@@ -9,17 +9,21 @@ defineProps({
   },
   options: {
     type: Array
-  }
+  },
+  modelValue: {},
 })
 </script>
 
 <template>
   <label :for="name" class="block mb-2 text-sm font-medium text-gray-900">{{name}}</label>
   <select :id="name"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-    <option selected>{{ defaultOption }}</option>
+    <option disabled  selected value="">{{ defaultOption }}</option>
     <option v-for="o in options"
-            :value="o.шв">{{o.name}}</option>
+            :key="o.id"
+            :value="o.id">{{o.name}}</option>
   </select>
 </template>
 

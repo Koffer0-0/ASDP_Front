616827b6e094ba7a52f29a3a071aa8d2dd4d56ab
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-28 12:00:57 +0500

    fix endpoint

--- asdpfront/constants/consts.ts
+++ asdpfront/constants/consts.ts
@@ -4,4 +4,4 @@ export const API_BASE_ENDPOINT = API_URL;
 export const API_AUTHORIZE_ENDPOINT = API_URL + 'Authorize';
 export const API_DOCUMENTS_ENDPOINT = API_URL + 'Documents';
 export const API_EMPLOYEES_ENDPOINT = API_URL + 'Employees';
-export const API_SIGN_ENDPOINT = API_URL + '/Sign';
+export const API_SIGN_ENDPOINT = API_URL + 'Sign';

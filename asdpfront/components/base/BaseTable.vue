7645ee0f78540ac45d4113a1a0b27501587faae1
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-28 11:59:47 +0500

    show items based on label keys

--- asdpfront/components/base/BaseTable.vue
+++ asdpfront/components/base/BaseTable.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
-defineProps({
-  lables: {},
-  items: {},
-})
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  labels: {
+    type: Array,
+    required: true,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
 
 <template>
@@ -19,30 +27,19 @@ defineProps({
         <span v-else>{{ label.text }}</span>
       </th>
       <th scope="col" class="p-4 text-xs font-semibold uppercase">
-        <span class="sr-only"> Actions </span>
+        <span class="sr-only">Actions</span>
       </th>
     </tr>
     </thead>
 
     <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
     <tr v-for="item in items" :key="item.id" class="hover:bg-gray-50 dark:hover:bg-gray-700">
-      <th scope="row" class="whitespace-nowrap p-4 font-medium text-gray-900 dark:text-white">
-        <a href="#" class="hover:underline">{{ item.id }}</a>
-      </th>
-      <td class="whitespace-nowrap p-4 text-sm font-medium">{{ item.doc_name }}</td>
-      <td class="whitespace-nowrap p-4 text-sm font-medium">{{ item.whom }}</td>
-      <td class="whitespace-nowrap p-4 text-sm font-medium">{{ item.dueDate }}</td>
-      <td class="p-4">
-          <span :class="item.statusClass" class="inline-flex items-center rounded px-2.5 py-0.5 text-xs font-medium">
-            <svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="item.statusIcon"/>
-            </svg>
-            {{ item.statusText }}
-          </span>
+      <!-- Dynamically render each cell based on the label's key -->
+      <td v-for="label in labels" :key="label.key" class="whitespace-nowrap p-4 text-sm font-medium">
+        {{ item[label.key] }}
       </td>
       <td class="p-4 text-right">
-        <slot name="actions">
-
+        <slot name="actions" :item="item">
         </slot>
       </td>
     </tr>
@@ -51,5 +48,5 @@ defineProps({
 </template>
 
 <style scoped>
-
+/* Custom styles */
 </style>

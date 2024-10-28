0db437073c5fcf806fb18ca88a28300ef5a1ff13
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2024-10-28 16:59:17 +0500

    add some padding bottom

--- asdpfront/layouts/default.vue
+++ asdpfront/layouts/default.vue
@@ -6,7 +6,7 @@
   <div>
     <the-navbar/>
     <the-sidebar/>
-      <main class="p-4 md:ml-64 h-screen pt-20 bg-gray-100">
+      <main class="p-4 md:ml-64 h-screen pt-20 mb-10">
         <NuxtPage />
       </main>
   </div>
